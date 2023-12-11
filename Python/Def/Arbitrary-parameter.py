7d36894e995a07a9661653f2fa88a1d936fbecec
Matidza M.Z <125007667+Matidza@users.noreply.github.com>
2023-12-11 18:30:39 +0000

    Basics- Def

---
+++ Python/Def/Arbitrary-parameter.py
@@ -0,0 +1,6 @@
+def trip(Team, students= 'string'):
+    string = input("Enter elements (Space-Seperated): ")
+    lst = str(string).split()
+    a = print( Team + "-" +  lst)
+    return a
+trip('Team-1')
